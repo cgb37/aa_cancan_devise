ecbce480058d89fed811d72f85714723dec401c5
Charles <charlesbrownroberts@miami.edu>
2015-03-27 10:05:50 -0400

    added user reference to articles table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150326153859) do
+ActiveRecord::Schema.define(version: 20150327140208) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -51,7 +51,10 @@ ActiveRecord::Schema.define(version: 20150326153859) do
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "articles", ["user_id"], name: "index_articles_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
